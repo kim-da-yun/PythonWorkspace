5b4e091f2dc3d2a6a1b5ff2adb3491814e62b613
kimdayun <day73299@naver.com>
2021-09-17 11:14:22 +0900

    숫자, 문자열, boolean 자료형 / 변수, 퀴즈1

---
+++ Quiz subway.py
@@ -0,0 +1,7 @@
+station = "사당"
+station2 = "신도림"
+station3 = "인천공항"
+
+print(station + "행 열차가 들어오고 있습니다.")
+print(station2 + "행 열차가 들어오고 있습니다.")
+print(station3 + "행 열차가 들어오고 있습니다.")
---
+++ prctice.py
@@ -0,0 +1,32 @@
+# 숫자 자료형
+print(5)
+print(-10)
+print(3.14)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+# 문자열 자료형
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋ")
+
+#boolean 자료형 (참 / 거짓)
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(not True)
+print(not (5 > 10))
+
+#변수
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+
+print("우리집 " + animal + "의 이름은" + name + "예요")
+hobby = "공놀이"
+print(name +"는" + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
